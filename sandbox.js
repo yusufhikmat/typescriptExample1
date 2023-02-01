f665e8b77c9e071b25648a016f04e4fcb336628e
yusufhikmat <hikmatyusuff2020@gmail.com>
2023-02-01 03:28:02 -1200

    first commit

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var character = "mario";
+//character = 20;
+character = "hikmah";
+var age = 20;
+var isBlue = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(20));
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+let character = "mario"
+//character = 20;
+character="hikmah"
+
+let age = 20
+let isBlue= true;
+
+const circ = (diameter)=>{
+    return diameter * Math.PI
+    
+}
+console.log(circ(20))
